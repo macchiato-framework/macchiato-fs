dbeed2bb05149d9265be2caa5462e3e36c357d88
Ricardo J. Mendez <ricardo@numergent.com>
2016-12-16 13:40:39 +0200

    Bumping development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macchiato/fs "0.0.3"
+(defproject macchiato/fs "0.0.4-SNAPSHOT"
   :description "filesystem utilities"
   :url "https://github.com/yogthos/macchiato-framework/macchiato-fs"
   :scm {:name "git"
